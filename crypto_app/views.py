93f405ee24746444d796e99afcc08698bc46594f
Sambiase <marsamb@gmail.com>
2023-01-30 20:51:50 -0300

    added new files

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import requests
+
 
 def crypto_view(request):
-    return render(request,'index.html')
+    cryptos = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()
+    top_ten = []
+    for i in range(0,11):
+        top_ten.append(cryptos[i])
+    context = {
+        "top_ten": top_ten
+    }
+    return render(request,'index.html', context)
