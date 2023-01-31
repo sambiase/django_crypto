25671e5c9140961ff2856b769b5ef36b4f329158
Sambiase <marsamb@gmail.com>
2023-01-31 18:30:17 -0300

    new files added

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'crypto_app',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- crypto_app/views.py
+++ crypto_app/views.py
@@ -4,10 +4,10 @@ import requests
 
 def crypto_view(request):
     cryptos = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()
-    top_ten = []
-    for i in range(0,11):
-        top_ten.append(cryptos[i])
+    data = []
+    for i in range(0,10):
+        data.append(cryptos[i])
     context = {
-        "top_ten": top_ten
+        "data": data
     }
     return render(request,'index.html', context)
